94711a8a338443359d7315ffc1251f9e6adefc73
lby0615 <bjl5716@psu.edu>
2020-09-03 15:55:33 +0000

    spelling mistakes

--- main.c
+++ main.c
@@ -6,6 +6,6 @@ int main(void) {
 char *degree = readline ("Enter temperature in celsius: ");
 double c = atof(degree);
 double f = 1.8*c+32.0;
-printf("%.6f° in Celsius is equavalent to %.6f° Fahrenheit.\n",c,f);
+printf("%.6f° in Celsius is equivalent to %.6f° Fahrenheit.\n",c,f);
   return 0;
 }
